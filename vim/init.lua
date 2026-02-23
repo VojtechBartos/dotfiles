038d3d8629dc4187e889246eccc4f20823dfd431
Vojta Bartos <hi@vojtabartos.com>
2026-02-23 22:34:28 +0100

    Fixing neotree resizing

--- vim/init.lua
+++ vim/init.lua
@@ -98,6 +98,10 @@ vim.keymap.set("n", "<leader>r", "<cmd>Ag<cr>", { desc = "Full text search (silv
 
 -- Neo-tree: open/toggle file explorer; show hidden files and directories
 require("neo-tree").setup({
+  window = {
+    width = 35,
+    adaptive_size = false,
+  },
   filesystem = {
     filtered_items = {
       hide_dotfiles = false,
@@ -117,13 +121,10 @@ vim.keymap.set("n", "<leader>s", "<cmd>w<cr>", { desc = "Save" })
 -- Tabs
 vim.keymap.set("n", "<leader>c", "<cmd>tabnew<cr>", { desc = "New tab" })
 
-vim.opt.fillchars = { vert = "│", vertleft = "│", vertright = "│" }
 
 local function set_indent_highlights()
   vim.api.nvim_set_hl(0, "IBLChar",      { fg = "#3b414d" })
   vim.api.nvim_set_hl(0, "IblScope",     { fg = "#5a6070" })
-  vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#7a8499", bg = "none" })
-  vim.api.nvim_set_hl(0, "VertSplit",    { fg = "#7a8499", bg = "none" })
 end
 set_indent_highlights()
 vim.api.nvim_create_autocmd("ColorScheme", { callback = set_indent_highlights })
