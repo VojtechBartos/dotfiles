64b718c01d13af1cd7dc1b6372ed1d092c7b56f8
Vojta Bartos <hi@vojtabartos.com>
2026-02-25 21:30:54 +0100

    Tmux config improvement with mouse work

--- terminal/install.sh
+++ terminal/install.sh
@@ -2,6 +2,11 @@
 DOTFILES_ROOT="$(cd "$(dirname "$0")/.." && pwd)"
 
 mkdir -p "$HOME/.config"
+
+# Install tmux plugin manager
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+  git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
+fi
 if [ "$(uname -s)" = "Darwin" ]; then
   mkdir -p "$HOME/Library/Application Support/com.mitchellh.ghostty"
   ln -sf "$DOTFILES_ROOT/terminal/ghostty.config.symlink" \
